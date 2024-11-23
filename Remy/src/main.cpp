deed9a7a7e35574202081862a7effafc524569bd
Dzau <102695761+Dzau@users.noreply.github.com>
2024-11-23 15:49:03 -0500

    Update main.cpp

--- Remy/src/main.cpp
+++ Remy/src/main.cpp
@@ -2,25 +2,49 @@
 #include <kipr/wombat.h>
 #include <Remy.h>
 
+//constants for robot speed
+const float slow = 200;
+const float medium = 400;
+const float fast = 800;
+//constants for sensor ports
+const float front = 0;
+const float left_side = 1;
+const float right_side = 2;
 void p(std::string out) {
     std::cout << out << std::endl;
 }
 
 int main(){
+    //initialize robot
     Remy robot;
     
-    p("Moving forward");
-
-    robot.move_distance(5, 1);
-    robot.stop();
+    //move out of start box and to botguy
+    //move until you hit the front sensor on pvc pipe in front of botguy
+    robot.move_till_sensor(front, medium);
     
-    p("Waiting 3 seconds");
-    msleep(3000);
+    //backup to position arm to grab botguy
+    robot.move_distance(-1, medium);
 
-    p("Sideways for 3 seconds")
-    robot.sideways_cont(1);
-    msleep(3000);
+    //bring down arm of claw to the height of botguy
+    robot.set_elbow(800);
 
-    robot.stop();
+    //set the claw closed around botguys eyes
+    robot.set_claw(200);
+
+    //raise arm with buy in grasp
+    robot.set_elbow(1600);
+
+    //move backwards to area to drop off botguy
+    robot.move_distance(-30,slow);
+
+    //turn towards stove
+    robot.rotate(-120);
+
+    //move towards stove
+    robot.move_distance(4, slow);
+
+    //drop off botguy
+    robot.open_claw();
+
 
 }
